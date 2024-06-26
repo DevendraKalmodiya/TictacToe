ea77d57735fc48cbbe029a04ca5a9df8ce58c51f
DevendraKalmodiya <devendrakalmodiya1@gmail.com>
2024-06-26 13:30:52 +0530

    Add files via upload
    I created a Tic-Tac-Toe game from scratch using HTML, CSS, and JavaScript to demonstrate my expertise in front-end web development. This project features responsive gameplay across various devices, allowing users to interact by placing Xs and Os with simple clicks or taps. I implemented robust win detection logic in JavaScript to determine game outcomes and manage the game state effectively.

---
+++ Script.js
@@ -0,0 +1,66 @@
+let container = document.querySelector(".container");
+let boxes = document.querySelectorAll("#box");
+let turnO = true;
+let msgContainer = document.querySelector(".msg-container");
+let msg = document.querySelector("#msg");
+let newGameBtn = document.querySelector("#newGame");
+let resetBtn = document.querySelector("#reset");
+const winPatterns = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+boxes.forEach((box) => {
+  box.addEventListener("click", () => {
+    if (turnO) {
+      box.innerText = "O";
+      turnO = false;
+    } else {
+      box.innerText = "X";
+      turnO = true;
+    }
+    box.disabled = true;
+    checkWinner();
+  });
+});
+const disableboxes = () => {
+  for (let box of boxes) {
+    box.disabled = true;
+  }
+};
+const enableboxes = () => {
+  for (let box of boxes) {
+    box.disabled = false;
+    box.innerText = "";
+  }
+};
+const resetGame = () => {
+  turnO = true;
+  enableboxes();
+  msgContainer.classList.add("hide");
+};
+const showWinner = (winner) => {
+  msg.innerText = `Congratulations, Winner is ${winner}`;
+  msgContainer.classList.remove("hide");
+  disableboxes();
+};
+const checkWinner = () => {
+  for (let pattern of winPatterns) {
+    let pos1val = boxes[pattern[0]].innerText;
+    let pos2val = boxes[pattern[1]].innerText;
+    let pos3val = boxes[pattern[2]].innerText;
+    if (pos1val != "" && pos2val != "" && pos3val != "") {
+      if (pos1val == pos2val && pos2val == pos3val) {
+        console.log("Winner");
+        showWinner(pos1val);
+      }
+    }
+  }
+};
+newGameBtn.addEventListener("click", resetGame);
+resetBtn.addEventListener("click", resetGame);
